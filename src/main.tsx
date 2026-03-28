035ca88d068a4d3230f58437840119a9ee5020aa
ozer <30857654+tsiganin@users.noreply.github.com>
2026-03-28 23:25:01 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ThemeProvider } from 'next-themes';
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );
